9861bdc8259c04980e4be59aebb1a5383a94a675
JeremyPflipsen <jeremypflipsen@gmail.com>
2023-06-12 22:25:46 -0500

    fixing test class

--- src/test/java/com/example/githubactionsdemo/GithubactionsdemoApplicationTests.java
+++ src/test/java/com/example/githubactionsdemo/GithubactionsdemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@SpringBootTest(classes = GithubactionsdemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GithubactionsdemoApplicationTests {
 
 	@LocalServerPort
