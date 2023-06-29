e753c96b4d5c39fd79df1507dec400e101a063fd
JeremyPflipsen <jeremypflipsen@gmail.com>
2023-06-29 10:12:55 -0500

    Revert "Revert "adding testcontainers""
    This reverts commit f2973ba237eb1dd1d3838eff6448447851a1abd1.

---
+++ src/main/java/com/example/githubactionsdemo/domain/Product.java
@@ -0,0 +1,37 @@
+package com.example.githubactionsdemo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_generator")
+    @SequenceGenerator(name = "product_id_generator", sequenceName = "product_id_seq")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+}
