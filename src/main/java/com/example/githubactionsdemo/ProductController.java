577c4bbdb54319718c4d3e4255b427d17fb725f7
JeremyPflipsen <jeremypflipsen@gmail.com>
2023-06-29 09:57:08 -0500

    deleteme

---
+++ src/main/java/com/example/githubactionsdemo/ProductController.java
@@ -0,0 +1,33 @@
+/* (C)2023 */
+package com.example.githubactionsdemo;
+
+import com.example.githubactionsdemo.domain.Product;
+import com.example.githubactionsdemo.domain.ProductEventPublisher;
+import com.example.githubactionsdemo.domain.ProductRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+class ProductController {
+    private final ProductRepository productRepository;
+    private final ProductEventPublisher productEventPublisher;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createProduct(@RequestBody Product product) {
+        productEventPublisher.publishProductCreatedEvent(product);
+        return ResponseEntity.ok().build();
+    }
+}
